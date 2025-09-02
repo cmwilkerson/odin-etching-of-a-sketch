cc035bb0bc881f595831fe2a08853ea601263bcf
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-09-01 21:00:14 -0700

    trying to get mouseover and mouseout to work

--- javascript-file.js
+++ javascript-file.js
@@ -1,19 +1,25 @@
+ 
 let playableGrid= document.querySelector("#gridContainer");
-console.log(playableGrid);
 let elementCreation
 function buildGrid(sideLength){
     for(let r = 1; r <= sideLength; r++ ) {
     for(let c = 1; c <= sideLength; c++ ){
-        console.log(`Current Row: ${r} Current Column:${c}`);
         elementCreation=document.createElement("div");
         elementCreation.setAttribute("id",`divR${r}C${c}`);
         elementCreation.classList.add("gridPlay");
-        elementCreation.textContent=`R${r}C${c}`;
         elementCreation.style.flex =`1 1 ${(100/sideLength)}%`;
+        
+        elementCreation.addEventListener('mouseout', () => {
+            elementCreation.style.backgroundColor= 'red';
+        })
+
+
+
         playableGrid.appendChild(elementCreation);
     }    
 }
 }
-buildGrid(10);
+
+buildGrid(2);
 console.log(playableGrid);
 
